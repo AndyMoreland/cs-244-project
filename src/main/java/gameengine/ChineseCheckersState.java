91b75793d6ee70c8eb5c3b322683edc3bb6219c3
Andy Moreland <andy@andymo.org>
2014-11-30 02:10:42 -0800

    Add new constructor to chinese checkers state

--- src/main/java/gameengine/ChineseCheckersState.java
+++ src/main/java/gameengine/ChineseCheckersState.java
@@ -10,9 +10,25 @@ public class ChineseCheckersState {
     private final List<Player> players;
     private int currentPlayerIndex;
 
+    /**
+     * Constructs the state for a fresh chinese checkers game
+     * @param players
+     */
     public ChineseCheckersState(List<Player> players) {
         this.players = players;
         currentPlayerIndex = 0;
+    }
+
+    /**
+     * Designed for entering an in-progress game
+     * @param board
+     * @param players
+     * @param currentPlayerIndex
+     */
+    public ChineseCheckersState(ChineseCheckersBoard board, List<Player> players, int currentPlayerIndex) {
+        this.board = board;
+        this.players = players;
+        this.currentPlayerIndex = currentPlayerIndex;
     }
 
     public Player getCurrentPlayer() {
