62f3010fde9b4d6e13865c25b94761717c742ba1
Susan Tu <susanctu@gmail.com>
2014-11-30 15:43:46 -0800

    wip

--- src/main/java/config/GroupConfigProvider.java
+++ src/main/java/config/GroupConfigProvider.java
@@ -6,9 +6,16 @@ import java.util.Set;
  * Created by andrew on 11/27/14.
  */
 public interface GroupConfigProvider {
+
+    void setViewID(int viewID);
+    int getViewID();
+    int getQuorumSize();
+
     Set<GroupMember> getGroupMembers();
 
     GroupMember getLeader();
 
     void setLeader(GroupMember leader);
+
+
 }
