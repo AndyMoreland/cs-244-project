9bdd990380d1b6a75765e0c87d43a9730645a85c
Susan Tu <susanctu@gmail.com>
2014-11-30 20:56:07 -0800

    headers for crypto stuff

---
+++ src/main/java/common/CryptoUtil.java
@@ -0,0 +1,40 @@
+package common;
+
+import PBFT.*;
+
+/**
+ * Created by sctu on 11/30/14.
+ */
+public final class CryptoUtil {
+    private CryptoUtil() {
+        // don't instantiate
+    }
+
+    public static byte[] computeTransactionDigest(Transaction transaction) {
+
+    }
+
+    public static byte[] computePrePrepareSignature(PrePrepareMessage message) {
+
+    }
+
+    public static byte[] computePrepareSignature(PrepareMessage message) {
+
+    }
+
+    public static byte[] computeCommitSignature(CommitMessage message) {
+
+    }
+
+    public static byte[] computeCheckpointSignature(CheckpointMessage message) {
+
+    }
+
+    public static byte[] computeViewChangeSignature(ViewChangeMessage message) {
+
+    }
+
+    public static byte[] computeNewViewSignature(NewViewMessage message) {
+
+    }
+}
