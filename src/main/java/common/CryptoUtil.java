87b2ffc5ca84ac73d0160ee6f181cb404b080e78
Susan Tu <susanctu@gmail.com>
2014-11-30 21:46:29 -0800

    message digest

--- src/main/java/common/CryptoUtil.java
+++ src/main/java/common/CryptoUtil.java
@@ -1,5 +1,6 @@
 package common;
 
+import org.codehaus.jackson.JsonGenerationException;
 import org.codehaus.jackson.map.ObjectMapper;
 import org.codehaus.jackson.map.ObjectWriter;
 import org.codehaus.jackson.map.ser.FilterProvider;
@@ -7,6 +8,8 @@ import org.codehaus.jackson.map.ser.impl.SimpleBeanPropertyFilter;
 import org.codehaus.jackson.map.ser.impl.SimpleFilterProvider;
 
 import java.io.IOException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.security.Signature;
 import java.security.SignatureException;
 
@@ -14,12 +17,11 @@ import java.security.SignatureException;
  * Created by sctu on 11/30/14.
  */
 public final class CryptoUtil {
-    private static final ObjectMapper mapper = new ObjectMapper();
+    private static final ObjectMapper mapper = new ObjectMapper(); // thread-safe
     private static final String[] IGNORED_FIELD = {"messageSignature"};
     private static final FilterProvider FIELD_FILTER = new SimpleFilterProvider()
             .addFilter("pre-prepare filter",
                     SimpleBeanPropertyFilter.serializeAllExcept(IGNORED_FIELD));
-
 
     private CryptoUtil() {
         // don't instantiate
@@ -30,8 +32,10 @@ public final class CryptoUtil {
         return sig.sign();
     }
 
-    public static byte[] computeTransactionDigest(Transaction transaction) {
-        return null; // TODO
+    public static byte[] computeTransactionDigest(Transaction transaction) throws NoSuchAlgorithmException, IOException {
+        ObjectWriter writer = mapper.writer();
+        MessageDigest digest = MessageDigest.getInstance("SHA-256");
+        return digest.digest(writer.writeValueAsString(transaction).getBytes());
     }
 
     public static byte[] computeMessageSignature(Object message, Signature sig) throws IOException, SignatureException {
