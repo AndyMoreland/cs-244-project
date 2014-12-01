b8b2db3ac33c246e181b8e6ac3827541864e2a30
Leo Martel <lmartel@github>
2014-11-30 18:49:52 -0800

    started tests

--- src/main/src/test/java/gameengine/ChineseCheckersTest.java
+++ src/main/src/test/java/gameengine/ChineseCheckersTest.java
@@ -1,9 +1,12 @@
 package gameengine;
 
+import gameengine.operations.MovePiece;
 import org.junit.Test;
 
 import java.util.ArrayList;
 import java.util.List;
+
+import static org.junit.Assert.*;
 
 /**
  * Created by leo on 11/30/14.
@@ -26,6 +29,16 @@ public class ChineseCheckersTest {
 
     @Test
     public void testMoveValidity(){
+        MovePiece moveEmptyHex = new MovePiece(0, 0, 1, 0);
+        MovePiece moveOutOfBounds = new MovePiece(3, -7, 2, -7);
+        MovePiece moveOntoOtherPiece = new MovePiece(3, -5, 4, -5);
+        MovePiece goodMove = new MovePiece(3, -5, 3, -4);
+        MovePiece goodJumpMove = new MovePiece(2, -6, 2, -4);
 
+        assertFalse(moveEmptyHex.isValid(this.game));
+        assertFalse(moveOutOfBounds.isValid(this.game));
+        assertFalse(moveOntoOtherPiece.isValid(this.game));
+        assertTrue(goodMove.isValid(this.game));
+        assertTrue(goodJumpMove.isValid(this.game));
     }
 }
