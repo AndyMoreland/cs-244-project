dd3a55c22a4a9ec35498ed3d18b3bf72ae3f1c3a
Andy Moreland <andy@andymo.org>
2014-11-30 17:07:21 -0800

    Add operation factory

---
+++ src/main/java/gameengine/ChineseCheckersOperationFactory.java
@@ -0,0 +1,24 @@
+package gameengine;
+
+import gameengine.operations.AddPlayer;
+import gameengine.operations.ChineseCheckersOperation;
+import gameengine.operations.KickPlayer;
+import gameengine.operations.MovePiece;
+
+/**
+ * Created by andrew on 11/30/14.
+ */
+public class ChineseCheckersOperationFactory {
+    public static ChineseCheckersOperation hydrate(PBFT.Operation op) {
+        PBFT.ChineseCheckersOperation type = PBFT.ChineseCheckersOperation.findByValue(op.getOperationType());
+
+        switch (type) {
+            case MOVE_PIECE:
+                return new MovePiece();
+            case KICK_PLAYER:
+                return new KickPlayer();
+            case ADD_PLAYER:
+                return new AddPlayer();
+        }
+    }
+}
